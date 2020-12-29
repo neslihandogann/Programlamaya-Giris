767950afd30b4f4236fc0ac817cd8b4a301b473a
Burhan Mutlu <72438811+burhanmutlu@users.noreply.github.com>
2020-12-29 23:39:48 +0300

    Çarpım Tablosu

---
+++ carpimtablosu.cpp
@@ -0,0 +1,13 @@
+#include<iostream>//carpim tablosu
+using namespace std;
+int main(){
+	
+	
+		for(int i=0;i<10;i++){
+			for(int j=0;j<10;j++){
+				cout<<i<<"*"<<j<<" ="<<i*j<<"	";
+			}
+			cout<<endl;
+		}
+		
+}
