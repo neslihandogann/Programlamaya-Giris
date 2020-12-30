f9eaca8e3259e912c3ef0c51d3632695a0d87c7d
Burhan Mutlu <72438811+burhanmutlu@users.noreply.github.com>
2020-12-31 00:56:11 +0300

    Değişken Boyutu Bastırma

---
+++ degiskenboyutu.cpp
@@ -0,0 +1,13 @@
+#include<iostream>//deðiþken boyutlarý
+using namespace std;
+int main(){
+	
+	
+	cout<<"tam sayi boyutu: "<<sizeof(int)<<endl;
+	cout<<"kisa tam sayi boyutu: "<<sizeof(short int)<<endl;
+	cout<<"uzun tam sayi boyutu: "<<sizeof(long int)<<endl;
+	cout<<"kesirli sayi boyutu: "<<sizeof(float)<<endl;
+	cout<<"uzun kesirli sayi boyutu: "<<sizeof(double)<<endl;
+	cout<<"karakter boyutu: "<<sizeof(char)<<endl;
+	cout<<"karakter dizisi boyutu: "<<sizeof(string)<<endl;
+}
