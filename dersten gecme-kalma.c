02c642801a75e927f5a5fa0cf34b5ae6f59df268
Burhan Mutlu <72438811+burhanmutlu@users.noreply.github.com>
2020-12-31 01:10:34 +0300

    Basit Dersten Geçme/Kalma

---
+++ dersten gecme-kalma.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+	label;
+	int x;
+	printf("notunuzu girin:");
+	scanf("%d",&x);
+	
+	if (x>=60 && x<=100){ 
+	printf("dersten gectiniz.");
+}
+	else if(x<60 ){
+	printf("derten kaldiniz.");
+}
+	
+	  else {
+		
+		printf("hatali giris.");
+}
+
+	go to label;
+	return 0;
+}
