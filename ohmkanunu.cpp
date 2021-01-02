ce54f869b5254924408cac98fca32ab5a53ae0a1
Burhan Mutlu <72438811+burhanmutlu@users.noreply.github.com>
2021-01-03 00:44:55 +0300

    ohm Kanunu

---
+++ ohmkanunu.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	float R,I,V;
+	cout<<"iletkeninin direncini giriniz:"<<endl;
+	cin>>R;
+	cout<<"iletkeninin akimini giriniz:"<<endl;
+	cin>>I;
+	V=R*I;
+	cout<<"Gerilim: "<<V;
+}
